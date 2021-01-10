feb9e1b1e97a59f3551d5f644c73b2471cc66201
ncku_workstation_khduh <dushiun@gmail.com>
2021-01-10 17:14:22 +0800

    remain fc and max

--- compare_result_rtl.sv
+++ compare_result_rtl.sv
@@ -7,28 +7,28 @@ module compare_result(
 input        [159:0] input_data;
 output logic [31:0]compare_result;
 
-logic [15:0] data1;
-logic [15:0] data2;
-logic [15:0] data3;
-logic [15:0] data4;
-logic [15:0] data5;
-logic [15:0] data6;
-logic [15:0] data7;
-logic [15:0] data8;
-logic [15:0] data9;
+logic signed [15:0] data1;
+logic signed [15:0] data2;
+logic signed [15:0] data3;
+logic signed [15:0] data4;
+logic signed [15:0] data5;
+logic signed [15:0] data6;
+logic signed [15:0] data7;
+logic signed [15:0] data8;
+logic signed [15:0] data9;
 
 
 always_comb
 begin
-	data1=(input_data[159:144]>=input_data[143:128])?input_data[159:144]:input_data[143:128];
-	data2=(input_data[127:112]>=input_data[111:96])?input_data[127:112]:input_data[111:96];
-	data3=(input_data[95:80]>=input_data[79:64])?input_data[95:80]:input_data[79:64];
-	data4=(input_data[63:48]>=input_data[47:32])?input_data[63:48]:input_data[47:32];
-	data5=(input_data[31:16]>=input_data[15:0])?input_data[31:16]:input_data[15:0];
-	data6=(data1>=data2)?data1:data2;
-	data7=(data3>=data4)?data3:data4;
-	data8=(data6>=data7)?data6:data7;
-	data9=(data8>=data5)?data8:data5;
+	data1=(signed'(input_data[159:144])>=signed'(input_data[143:128]))?input_data[159:144]:input_data[143:128];
+	data2=(signed'(input_data[127:112])>=signed'(input_data[111:96]))?input_data[127:112]:input_data[111:96];
+	data3=(signed'(input_data[95:80])>=signed'(input_data[79:64]))?input_data[95:80]:input_data[79:64];
+	data4=(signed'(input_data[63:48])>=signed'(input_data[47:32]))?input_data[63:48]:input_data[47:32];
+	data5=(signed'(input_data[31:16])>=signed'(input_data[15:0]))?input_data[31:16]:input_data[15:0];
+	data6=(signed'(data1)>=signed'(data2))?data1:data2;
+	data7=(signed'(data3)>=signed'(data4))?data3:data4;
+	data8=(signed'(data6)>=signed'(data7))?data6:data7;
+	data9=(signed'(data8)>=signed'(data5))?data8:data5;
 	case(data9)
 		input_data[159:144]:
 		begin
